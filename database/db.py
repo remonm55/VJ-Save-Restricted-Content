1c74c80930469264e92cbd8d8b571617af78d3cf
remonm55 <remonm55@yahoo.com>
2025-03-12 23:32:03 +0200

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,27 +1,13 @@
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
-from motor.core import AgnosticClient
 
 class Database:
-    _client: AgnosticClient = None
-    
     def __init__(self):
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
         self.db = self.client[DB_NAME]
         self.col = self.db.users
 
-    @property
-    def client(self):
-        if self._client is None:
-            self._client = motor.motor_asyncio.AsyncIOMotorClient(
-                DB_URI,
-                maxPoolSize=100,
-                minPoolSize=10,
-                connectTimeoutMS=30000,
-                socketTimeoutMS=30000
-            )
-        return self._client
-    
-    # Keep all existing methods unchanged below
-    # ... [Rest of your existing methods]
+    # Keep all your existing methods below unchanged
+    # ... [Rest of your original db.py code here]
 
 db = Database()
