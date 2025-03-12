cba2ec3c1e46ed3b2942af35a794355e45868255
remonm55 <remonm55@yahoo.com>
2025-03-13 00:21:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,23 +14,16 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins=dict(root="TechVJ"),
-            workers=50,
-            sleep_threshold=10
+            workers=100,  # Increased workers for better concurrency
+            sleep_threshold=30  # Increased sleep threshold to reduce CPU usage
         )
 
-      
     async def start(self):
-            
         await super().start()
         print('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
-
         await super().stop()
         print('Bot Stopped Bye')
 
 Bot().run()
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
