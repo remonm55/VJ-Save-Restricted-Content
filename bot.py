f425210610359b812e45391bb19704b7a13ebc35
remonm55 <remonm55@yahoo.com>
2025-03-12 23:25:55 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,33 +4,29 @@
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
+import os
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            "techvj login",
+            name="techvj_login",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins=dict(root="TechVJ"),
-            workers=50,
-            sleep_threshold=10
+            workers=int(os.environ.get("WORKERS", 100)),
+            sleep_threshold=int(os.environ.get("SLEEP_THRESHOLD", 30)),
+            max_concurrent_transmissions=int(os.environ.get("MAX_CONCURRENT", 10)),
+            in_memory=True
         )
 
-      
     async def start(self):
-            
         await super().start()
         print('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
-
         await super().stop()
         print('Bot Stopped Bye')
 
-Bot().run()
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+if __name__ == "__main__":
+    Bot().run()
