9bc60b916e21356c1c24dcb25e9e10b6b75d37d9
remonm55 <remonm55@yahoo.com>
2025-03-13 01:53:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
@@ -10,16 +14,23 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins=dict(root="TechVJ"),
-            workers=100,
+            workers=50,
             sleep_threshold=10
         )
 
+      
     async def start(self):
+            
         await super().start()
         print('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
+
         await super().stop()
         print('Bot Stopped Bye')
 
 Bot().run()
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
