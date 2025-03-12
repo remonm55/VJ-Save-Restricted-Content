46e1c045e0dc2028af58ee740b332bf1a711df56
remonm55 <remonm55@yahoo.com>
2025-03-13 00:16:30 +0200

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,30 +2,30 @@ import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
 class Database:
-    def __init__(self):
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
-        self.db = self.client[DB_NAME]
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
         self.col = self.db.users
 
-    # Fixed method name to match exactly what's being called
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return bool(user)
-
-    # Keep all original methods below unchanged
     def new_user(self, id, name):
         return dict(
-            id=id,
-            name=name,
-            session=None
+            id = id,
+            name = name,
+            session = None,
         )
     
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
     
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id':int(id)})
+        return bool(user)
+    
     async def total_users_count(self):
-        return await self.col.count_documents({})
+        count = await self.col.count_documents({})
+        return count
 
     async def get_all_users(self):
         return self.col.find({})
@@ -34,14 +34,10 @@ class Database:
         await self.col.delete_many({'id': int(user_id)})
 
     async def set_session(self, id, session):
-        await self.col.update_one(
-            {'id': int(id)},
-            {'$set': {'session': session}}
-        )
+        await self.col.update_one({'id': int(id)}, {'$set': {'session': session}})
 
     async def get_session(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user.get('session') if user else None
+        return user.get('session')
 
-# Initialize database instance
-db = Database()
+db = Database(DB_URI, DB_NAME)
