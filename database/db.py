b2a0780b7aea7ba207451db617454bf19dd1ff77
remonm55 <remonm55@yahoo.com>
2025-03-12 23:45:10 +0200

    Update db.py

--- database/db.py
+++ database/db.py
@@ -7,7 +7,39 @@ class Database:
         self.db = self.client[DB_NAME]
         self.col = self.db.users
 
-    # Keep all your existing methods below unchanged
-    # ... [Rest of your original db.py code here]
+    async def is_user_exist(self, id):  # Added missing method
+        user = await self.col.find_one({'id': int(id)})
+        return bool(user)
+
+    # Keep all other original methods below
+    def new_user(self, id, name):
+        return dict(
+            id=id,
+            name=name,
+            session=None,
+        )
+    
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+    
+    async def total_users_count(self):
+        return await self.col.count_documents({})
+
+    async def get_all_users(self):
+        return self.col.find({})
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_session(self, id, session):
+        await self.col.update_one(
+            {'id': int(id)},
+            {'$set': {'session': session}}
+        )
+
+    async def get_session(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('session') if user else None
 
 db = Database()
