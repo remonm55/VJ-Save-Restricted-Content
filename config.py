1f9ba81949905c318b7f20aab844f693a700910c
remonm55 <remonm55@yahoo.com>
2025-03-12 23:21:51 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", "")
-ADMINS = int(os.environ.get("ADMINS", 0))
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "turbo_bot")
+# Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7702562393:AAF8xIGuvLgK39IuyqdvIpVROwX9zapzfOA")
 
-# Turbo Performance Settings
-MAX_CONCURRENT = 12
-CHUNK_SIZE = 50 * 1024 * 1024  # 50MB chunks
-REQUEST_DELAY = 0.5  # 500ms
-MAX_FILE_SIZE = 2048  # 2GB in MB
-MEMORY_LIMIT = 12288  # 12GB RAM buffer
-LOG_LEVEL = "INFO"
+# Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "27997082"))
+
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "6f3a423cc9f69c28ed9f99a42776bcf0")
+
+# Your Owner / Admin Id For Broadcast 
+ADMINS = int(os.environ.get("ADMINS", "1874350266"))
+
+# Your Mongodb Database Url
+# Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
