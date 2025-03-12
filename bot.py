c2463b8f9ad026883622d3f614f68349e0f6f276
remonm55 <remonm55@yahoo.com>
2025-03-12 23:21:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,36 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-logger = logging.getLogger(__name__)
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
-            "turbo_saver",
+            "techvj login",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins=dict(root="TechVJ"),
-            workers=16,
-            sleep_threshold=5,
-            max_concurrent_transmissions=12,
-            workdir="./sessions",
-            config_file="telegram.ini"
+            workers=50,
+            sleep_threshold=10
         )
 
+      
     async def start(self):
+            
         await super().start()
-        logger.info('⚡ TURBO MODE ACTIVATED ⚡')
-        
-    async def stop(self, *args):
-        await super().stop()
-        logger.info('Turbo Engine Stopped')
+        print('Bot Started Powered By @VJ_Botz')
 
-if __name__ == "__main__":
-    Bot().run()
+    async def stop(self, *args):
+
+        await super().stop()
+        print('Bot Stopped Bye')
+
+Bot().run()
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
