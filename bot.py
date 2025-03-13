1a9367aec56f819a075607171afbaec1364b424c
remonm55 <remonm55@yahoo.com>
2025-03-13 02:05:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,18 +14,15 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins=dict(root="TechVJ"),
-            workers=50,
+            workers=100,  # Increased workers for better performance
             sleep_threshold=10
         )
 
-      
     async def start(self):
-            
         await super().start()
         print('Bot Started Powered By @VJ_Botz')
 
     async def stop(self, *args):
-
         await super().stop()
         print('Bot Stopped Bye')
 
